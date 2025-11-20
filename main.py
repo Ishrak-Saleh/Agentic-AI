aba18f00fb0d691c1e4eb06101e4ad0d6ce378b0
ishrak-saleh <ischy2003@gmail.com>
2025-11-21 00:31:39 +0600

    Implemented basic LangGraph chatbot with Gemini model - Added StateGraph with chatbot node - Integrated google_genai:gemini-2.0-flash model - Added message reducer using add_messages - Created a basic message invocation and response handling

--- main.py
+++ main.py
@@ -1,6 +1,35 @@
-def main():
-    print("Hello from agenticai!")
+from dotenv import load_dotenv
+from typing import Annotated
 
+load_dotenv()
 
-if __name__ == "__main__":
-    main()
+from typing_extensions import TypedDict
+from langchain.chat_models import init_chat_model
+from langgraph.graph.message import add_messages
+from langgraph.graph import StateGraph, START, END
+
+from langchain.chat_models import init_chat_model
+
+llm = init_chat_model("google_genai:gemini-2.0-flash")
+#llm.invoke("Hello how are you?")
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
+    #add_messages is a reducer function, instead of overwriting values it accumulates msgs
+
+def chatbot(state: State) -> State:
+    return {"messages": [llm.invoke(state["messages"])]}
+
+builder = StateGraph(State)
+builder.add_node("chatbot_node", chatbot)
+
+builder.add_edge(START, "chatbot_node")
+builder.add_edge("chatbot_node", END)
+
+graph = builder.compile()
+
+message = {"role": "user", "content": "Hello!"}
+response = graph.invoke({"messages": [message]})
+response["messages"]
+
+print(response)
